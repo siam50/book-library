015250d971919cf7ad165a702e8254d74ebbd9d7
siam50 <siamurrahman50@gmail.com>
2023-04-05 23:41:45 +0600

    Home page banner created

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,33 @@
+import React from "react";
+import bannerAnnimation from "../../../assets/bannerAnnimation.json";
+import Lottie from "lottie-react";
+
+const Banner = () => {
+  return (
+    <div>
+      <section className="pt-10 overflow-hidden bg-gray-50 md:pt-0 sm:pt-16 2xl:pt-16">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="grid items-center grid-cols-1 md:grid-cols-2">
+            <div>
+              <h2 className="text-3xl text-accent font-bold leading-tight sm:text-4xl lg:text-5xl">
+                Hey 👋 Grow <br className="block sm:hidden" />
+                Your Knowledge
+              </h2>
+              <p className="max-w-lg mt-3 text-xl leading-relaxed text-gray-600 md:mt-8">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
+            </div>
+
+            <div className="relative">
+              <Lottie animationData={bannerAnnimation} className=" h-80" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <h1>This is Navbar</h1>
+      <div className="navbar bg-base-100 text-accent">
+        <div className="flex-1">
+          <Link to="/" className="btn btn-ghost normal-case text-3xl">
+            BookLibrary
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link className=" text-lg font-medium" to="/about">
+                About Us
+              </Link>
+            </li>
+            <li tabIndex={0}>
+              <Link>Parent</Link>
+            </li>
+            <li>
+              <Link>Item 3</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
